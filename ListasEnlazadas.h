d9b5e6ec9f4aaed2f8e4f14b8e380ccab7e53166
MahsaSimaei <97670499+MahsaSimaei@users.noreply.github.com>
2026-01-19 18:19:57 +0100

    iterativo
    iterativo

--- ListasEnlazadas.h
+++ ListasEnlazadas.h
@@ -1,4 +1,3 @@
-
 #ifndef LISTASENLAZADAS_H
 #define LISTASENLAZADAS_H
 
@@ -6,58 +5,86 @@
 #include <iostream>
 using namespace std;
 
-// Clase ListaEnlazada genérica usando plantillas
 template <typename T>
 class ListaEnlazada {
 private:
-    Nodo<T>* cabeza; // Puntero al primer nodo de la lista
+    Nodo<T>* cabeza; 
+
+    // --- MÉTODOS PRIVADOS RECURSIVOS (AYUDANTES) ---
+
+    // Ayudante para insertar: Busca el final llamándose a sí mismo
+    void insertarRecursivo(Nodo<T>* actual, T dato) {
+        // Caso base: Si el siguiente es nulo, insertamos aquí
+        if (actual->enlaceNodo() == nullptr) {
+            Nodo<T>* nuevoNodo = new Nodo<T>(dato);
+            actual->ponerEnlace(nuevoNodo);
+        } else {
+            // Paso recursivo: Seguimos buscando en el siguiente
+            insertarRecursivo(actual->enlaceNodo(), dato);
+        }
+    }
+
+    // Ayudante para mostrar: Imprime y llama al siguiente
+    void mostrarRecursivo(Nodo<T>* actual) const {
+        if (actual == nullptr) {
+            return; // Caso base: fin de la lista
+        }
+        // Asumimos que T es un puntero (Soldado*) y tiene mostrarInfo()
+        // Si T es un tipo básico, usa cout << actual->datoNodo()
+        // Para el examen, como son punteros a Soldado, idealmente:
+        // actual->datoNodo()->mostrarInfo(); 
+        
+        // Mantengo tu lógica genérica:
+        cout << actual->datoNodo() << " "; 
+        
+        mostrarRecursivo(actual->enlaceNodo()); // Paso recursivo
+    }
+
+    // Ayudante para borrar todo (Destructor)
+    void borrarTodoRecursivo(Nodo<T>* actual) {
+        if (actual == nullptr) return;
+        
+        // Primero borro el resto de la lista (post-orden)
+        borrarTodoRecursivo(actual->enlaceNodo());
+        
+        // Luego borro este nodo
+        delete actual;
+    }
 
 public:
     // Constructor
     ListaEnlazada() : cabeza(nullptr) {}
 
-    // Destructor
+    // Destructor Recursivo
     ~ListaEnlazada() {
-        Nodo<T>* actual = cabeza;
-        while (actual != nullptr) {
-            Nodo<T>* temp = actual;
-            actual = actual->enlaceNodo();
-            delete temp;
+        borrarTodoRecursivo(cabeza);
+    }
+
+    // Insertar un elemento al final (Recursivo)
+    void insertarAlFinal(T dato) {
+        // Caso especial: lista vacía
+        if (cabeza == nullptr) {
+            cabeza = new Nodo<T>(dato);
+        } else {
+            // Llamamos al ayudante recursivo
+            insertarRecursivo(cabeza, dato);
         }
     }
 
-    // Obtener la cabeza de la lista
+    // Imprimir todos los elementos (Recursivo)
+    void mostrarLista() const {
+        mostrarRecursivo(cabeza);
+        cout << endl;
+    }
+    
+    // Obtener la cabeza (necesario para otras funciones recursivas externas)
     Nodo<T>* getCabeza() const {
         return cabeza;
     }
-
-    // Modificar la cabeza de la lista
-    void setCabeza(Nodo<T>* nuevaCabeza) {
-        cabeza = nuevaCabeza;
-    }
-
-    // Insertar un elemento al final
-    void insertarAlFinal(T dato) {
-        Nodo<T>* nuevoNodo = new Nodo<T>(dato);
-        if (cabeza == nullptr) {
-            cabeza = nuevoNodo;
-        } else {
-            Nodo<T>* actual = cabeza;
-            while (actual->enlaceNodo() != nullptr) {
-                actual = actual->enlaceNodo();
-            }
-            actual->ponerEnlace(nuevoNodo);
-        }
-    }
-
-    // Imprimir todos los elementos
-    void mostrarLista() const {
-        Nodo<T>* actual = cabeza;
-        while (actual != nullptr) {
-            cout << actual->datoNodo() << " ";
-            actual = actual->enlaceNodo();
-        }
-        cout << endl;
+    
+    // Método extra útil para el juego: Comprobar si está vacía
+    bool estaVacia() const {
+        return cabeza == nullptr;
     }
 };
 
